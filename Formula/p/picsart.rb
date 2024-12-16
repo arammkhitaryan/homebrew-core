ba9a20ed6917538215d09d9e84bd61290eb31d74
Aram Mkhitaryan <aram.mkhitaryan@picsart.com>
2024-12-16 15:28:03 +0400

    added the checksum and license

--- Formula/p/picsart.rb
+++ Formula/p/picsart.rb
@@ -2,7 +2,8 @@ class Picsart < Formula
  desc "Picsart Background Removal and Image Enhancement - Allows users to remove backgrounds and upscale images without losing quality."
  homepage "https://picsart.com"
  url "https://github.com/PicsArt/picsart-creative-apis-cli/releases/download/v1.0.0/picsart-1.0.0.tar.gz", :using => :curl
- sha256 "your_sha256_checksum_here"
+ sha256 "8c7d9d7bf74a9505b7044dcad09181a13bf0d6c23267bb2e4ef1097d0821a18f"
+ license "MIT" 
 
  def install
   system "./configure", "--prefix=#{prefix}"
